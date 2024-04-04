6fa7ddc44c4c9c19e2d50fae9442b555853ded0b
Roshan Singh <rosh.s568@gmail.com>
2024-04-05 02:06:12 +0530

    Create Orders Page Componenets

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -10,13 +10,12 @@ const Page = () => {
       <Navbar />
       <div className="pt-8">
         <div>
-          <h2 className="font-poppins text-2xl font-extrabold text-[#77248BB5] ml-4 md:ml-0">
+          <h2 className="font-poppins mb-4 text-2xl font-extrabold text-[#77248BB5] ml-4 md:ml-0">
             Orders
           </h2>
         </div>
-        <div className="p-16 min-w-full flex justify-center">
-          <div className="p-16 shadow-md flex justify-center border-2 rounded gap-4">
-            <TableMenu />
+        <div className="px-16 w-full flex justify-center">
+          <div className="p-16 shadow-md w-full flex justify-center border-2 rounded gap-16">
             <TableMenu />
             <TableMenu />
           </div>
--- src/components/table-menu/table-menu.tsx
+++ src/components/table-menu/table-menu.tsx
@@ -14,7 +14,7 @@ const TableMenu = () => {
               Order details for the selected table number
             </p>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
-               <OrderItem />
+              <OrderItem />
             </div>
           </div>
         </div>
@@ -24,9 +24,29 @@ const TableMenu = () => {
 
   return (
     <div>
-      {modal && <OrderModal />}
-      <button className="bg-orange-400 min-w-16 p-16 text-white font-bold text-2xl rounded-md shadow-lg">
-        Table 42
+      <button
+        onClick={() => setModal(!modal)}
+        className="min-w-16 p-16 text-white font-bold rounded-md shadow-xl"
+        style={{ backgroundColor: `${modal ? "#fff" : "#FF4D4D"}` }}
+      >
+        {modal ? (
+          <div className="w-80 h-28 flex text-black flex-col justify-center items-center">
+            <p className="text-2xl font-bold">Table Number 42</p>
+            <div>
+              <ol>
+                <li>Item 1</li>
+                <li>Item 1</li>
+                <li>Item 1</li>
+                <li>Item 1</li>
+              </ol>
+            </div>
+          </div>
+        ) : (
+          <div className="w-80 h-28 flex flex-col justify-center items-center">
+            <p className="text-2xl font-bold">Table Number</p>
+            <p className="p-8 text-6xl font-bold">42</p>
+          </div>
+        )}
       </button>
     </div>
   );
